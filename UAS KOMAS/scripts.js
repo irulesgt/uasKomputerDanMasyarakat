792fd3dc42015738bbab1ab3832a86399d9e1ee4
Chaerul Muzaky Arif Santoso <159033795+irulesgt@users.noreply.github.com>
2024-07-07 01:53:59 +0700

    Add files via upload

---
+++ UAS KOMAS/scripts.js
@@ -0,0 +1,10 @@
+function showContent(sectionId) {
+  const sections = document.querySelectorAll('.content-section');
+  sections.forEach(section => {
+    if (section.id === sectionId) {
+      section.style.display = 'block';
+    } else {
+      section.style.display = 'none';
+    }
+  });
+}
